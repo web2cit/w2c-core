d86dddb7149454683ca756ebedffe24c4daf2e19
Diego de la Hera <delahera@gmail.com>
2022-02-27 21:58:06 -0300

    Export HTTPResponseError

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Domain } from "./domain/domain";
 export { Webpage } from "./webpage/webpage";
 export { fallbackTemplate } from "./fallbackTemplate";
+export { HTTPResponseError } from "./errors";
